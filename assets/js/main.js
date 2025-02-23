87ecbceca741eff8a3d01163b9d82409985e37be
Guram <86122251+MasterGuram@users.noreply.github.com>
2025-02-23 14:07:42 +0400

    Change background Header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,15 @@
-/*=============== CHANGE BACKGROUND HEADER ===============*/
+/* CHANGE BACKGROUND HEADER */
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // When scroll is > 50 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/* SWIPER POPULAR */
 
 
-/*=============== SWIPER POPULAR ===============*/
-
-
-/*=============== VALUE ACCORDION ===============*/
+/* VALUE ACCORDION */
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
