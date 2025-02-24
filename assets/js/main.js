c42e9619426a016d8967772ae577397cbdb9e19e
Guram <86122251+MasterGuram@users.noreply.github.com>
2025-02-24 10:20:44 +0400

    Popular

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,18 +7,30 @@ function scrollHeader(){
 window.addEventListener('scroll', scrollHeader)
 
 /* SWIPER POPULAR */
+var swiperPopular = new Swiper(".popular__container", {
+    spaceBetween: 32,
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    loop: true,
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
 
 
 /* VALUE ACCORDION */
 
 
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+/* SCROLL SECTIONS ACTIVE LINK */
 
 
-/*=============== SHOW SCROLL UP ===============*/ 
+/* SHOW SCROLL UP */ 
 
 
-/*=============== DARK LIGHT THEME ===============*/ 
+/* DARK LIGHT THEME */ 
 
 
-/*=============== SCROLL REVEAL ANIMATION ===============*/
+/* SCROLL REVEAL ANIMATION */
