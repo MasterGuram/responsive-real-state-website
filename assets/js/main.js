b591e871cdfac13bb1cdc8ff09534bee8305e2d0
Guram <86122251+MasterGuram@users.noreply.github.com>
2025-02-28 13:04:05 +0400

    ScrollUp

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,10 +25,55 @@ var swiperPopular = new Swiper(".popular__container", {
 
 
 /* SCROLL SECTIONS ACTIVE LINK */
+const sections = document.querySelectorAll('section[id]')
 
+function scrollActive() {
+    const scrollY = window.scrollY;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id');
+              
+        const link = document.querySelector('.nav__menu a[href*="' + sectionId + '"]');
+        
+        if (link) {
+            if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+                link.classList.add('active-link');
+            } else {
+                link.classList.remove('active-link');
+            }
+        }
+    });
+}
+
+// Example: Add event listener to trigger function on scroll
+window.addEventListener('scroll', scrollActive);
+
+// const sections = document.querySelectorAll('section[id]')
+
+// function scrollActive(){
+//     const scrollY = window.pageYOffset
+
+//     sections.forEach(current =>{
+//         const sectionHeight = current.offsetHeight,
+//               sectionTop = current.offsetTop - 58,
+//               sectionId = current.getAttribute('id')
+              
+//     if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+//         document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+//     }else{
+//         document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+//     }
+//     })
+// }
 
 /* SHOW SCROLL UP */ 
-
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    if(this.scrollY >= 350) scrollUp.classList.add('show-scroll');
+}
+window.addEventListener('scroll', scrollUp)
 
 /* DARK LIGHT THEME */ 
 
